769178f1da83e75fc2479450a0a8aea14d20e59b
lqb <511946604@qq.com>
2020-01-02 10:11:27 +0800

    demo提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//zhushi666gg
+//zhushi66
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
